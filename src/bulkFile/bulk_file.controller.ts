7f0fe7c48fb0514c46c3f3e2edc659a04cfdbc6e
ACPR <yefimabramson@cvpcorp.com>
2022-05-26 14:14:59 -0400

    added endpoint post and put for bulk data file

--- src/bulkFile/bulk_file.controller.ts
+++ src/bulkFile/bulk_file.controller.ts
@@ -1,8 +1,10 @@
 import { ApiOkResponse, ApiSecurity, ApiTags } from '@nestjs/swagger';
 
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Put, Body, Param } from '@nestjs/common';
 
 import { BulkFileService } from './bulk_file.service';
+
+import { BulkFileInputDTO } from 'src/dto/bulk_file_input.dto';
 
 @ApiSecurity('APIKey')
 @ApiTags('Bulk Files')
@@ -27,4 +29,23 @@ export class BulkFileController {
   async postBulkFileMetadata() {
     return 'get bookmark';
   }
+
+  @Post('bulk-files')
+  @ApiOkResponse({
+    type: BulkFileInputDTO,
+    description: 'Posts a new database entry for an S3 file'
+  })
+  async postBulkFile(@Body() bulkDataFile: BulkFileInputDTO) {
+    return this.service.postBulkDataFile(bulkDataFile);
+  }
+
+  @Put('bulk-files/:id')
+  @ApiOkResponse({
+    type: BulkFileInputDTO,
+    description: 'Puts new information for a database entry for an S3 file'
+  })
+  async putBulkFile(@Param('id') id: number, @Body() bulkDataFile: BulkFileInputDTO) {
+    return this.service.putBulkDataFile(id, bulkDataFile);
+  }
+
 }
--- src/bulkFile/bulk_file.service.ts
+++ src/bulkFile/bulk_file.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Logger } from '@us-epa-camd/easey-common/logger';
 import { BulkFileDTO } from 'src/dto/bulk_file.dto';
+import { BulkFileInputDTO } from 'src/dto/bulk_file_input.dto';
 import { BulkFileMetadataRepository } from './bulk_file.repository';
 import { BulkFileMap } from '../maps/bulk-file-map';
 
@@ -16,5 +17,36 @@ export class BulkFileService {
 
   async getBulkDataFiles(): Promise<BulkFileDTO[]> {
     return this.map.many(await this.repository.find());
+  }
+
+  async postBulkDataFile(bulkFileDTO: BulkFileInputDTO): Promise<BulkFileDTO> {
+    var find = await this.repository.find({s3_key: bulkFileDTO.s3Path})
+    if (find.length == 0) {
+      var today = new Date();
+      var databaseinput = {
+        s3_key: bulkFileDTO.s3Path,
+        metadata: JSON.stringify(bulkFileDTO.metadata),
+        file_size: bulkFileDTO.bytes,
+        add_date: today,
+        last_update_date: today,
+      }
+      await this.repository.insert(databaseinput);
+      return this.map.one(await this.repository.findOne({s3_key: bulkFileDTO.s3Path}));
+    }
+    else {
+      return this.map.one(find[0]);
+    }
+  }
+
+  async putBulkDataFile(id: number, bulkFileDTO: BulkFileInputDTO): Promise<BulkFileDTO> {
+    var databaseinput = {
+      s3_key: bulkFileDTO.s3Path,
+      metadata: JSON.stringify(bulkFileDTO.metadata),
+      file_size: bulkFileDTO.bytes,
+      last_update_date: new Date(),
+    };
+    await this.repository.update(id, databaseinput);
+    var find = await this.repository.findOne({id: id})
+    return this.map.one(find);
   }
 }
