3d754d4a6e6e00117aebd4ae19b23c1b94b1ba2d
subrataroy1 <subrataroy@cvpcorp.com>
2023-05-27 16:53:23 -0400

    feat: add validation for DTO query params

--- src/entities/monitor-location.entity.ts
+++ src/entities/monitor-location.entity.ts
@@ -1,6 +1,15 @@
-import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryColumn,
+} from 'typeorm';
 
 import { NumericColumnTransformer } from '@us-epa-camd/easey-common/transforms';
+import { StackPipe } from './stack-pipe.entity';
+import { Unit } from './unit.entity';
 
 @Entity({ name: 'camdecmpswks.monitor_location' })
 export class MonitorLocation extends BaseEntity {
@@ -25,4 +34,12 @@ export class MonitorLocation extends BaseEntity {
 
   @Column({ name: 'update_date' })
   updateDate: string;
+
+  @OneToOne(() => StackPipe, (o) => o.location, { eager: true })
+  @JoinColumn({ name: 'stack_pipe_id' })
+  stackPipe: StackPipe;
+
+  @OneToOne(() => Unit, (o) => o.location, { eager: true })
+  @JoinColumn({ name: 'unit_id' })
+  unit: Unit;
 }
