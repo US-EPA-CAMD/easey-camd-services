adec6d1f4ab77b86e5ba8a84772cfcfe1d2feb68
Fahim Anwari <fahimanwari@cvpcorp.com>
2023-06-02 09:54:16 -0400

    Changed route to qa-maintenance

--- src/routes.ts
+++ src/routes.ts
@@ -43,7 +43,7 @@ const routes: Routes = [
     module: AdminModule,
     children: [
       {
-        path: '/qa-certification',
+        path: '/qa-maintenance',
         module: QaMaintenanceModule,
         children: [
           {
