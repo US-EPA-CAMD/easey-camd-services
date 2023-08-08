e40b1aa95d509654114b62437b0c15d0371269c6
Kyle-Herceg <kyleherceg@cvpcorp.com>
2023-08-08 10:20:26 -0400

    Fixed broken tests and added new ones

--- src/submission/submission-process.service.spec.ts
+++ src/submission/submission-process.service.spec.ts
@@ -7,6 +7,12 @@ import { SubmissionSet } from '../entities/submission-set.entity';
 import { SubmissionQueue } from '../entities/submission-queue.entity';
 import * as fsFunctions from 'fs';
 import { ReportDTO } from '../dto/report.dto';
+import { ConfigService } from '@nestjs/config';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { MonitorPlan } from '../entities/monitor-plan.entity';
+import { QaSuppData } from '../entities/qa-supp.entity';
+import { EmissionEvaluation } from '../entities/emission-evaluation.entity';
+import { Observable, of } from 'rxjs';
 
 describe('-- Submission Process Service --', () => {
   let service: SubmissionProcessService;
@@ -16,7 +22,17 @@ describe('-- Submission Process Service --', () => {
       imports: [LoggerModule],
       controllers: [],
       providers: [
+        ConfigService,
         SubmissionProcessService,
+        {
+          provide: HttpService,
+          useFactory: () => ({
+            post(url: string, data: any): Observable<any> {
+              // Simulate an HTTP POST request and return an observable
+              return of({ message: 'Mock HTTP response', data });
+            },
+          }),
+        },
         {
           provide: DataSetService,
           useFactory: () => ({
@@ -43,9 +59,17 @@ describe('-- Submission Process Service --', () => {
             return new SubmissionSet();
           case 'SubmissionQueue':
             return new SubmissionQueue();
+          case 'MonitorPlan':
+            return new MonitorPlan();
+          case 'QaSuppData':
+            return new QaSuppData();
+          case 'EmissionEvaluation':
+            return new EmissionEvaluation();
         }
         return false;
       }),
+
+      transaction: jest.fn(),
 
       find: jest.fn().mockResolvedValue([new SubmissionQueue()]),
 
@@ -63,20 +87,74 @@ describe('-- Submission Process Service --', () => {
     const record = new SubmissionQueue();
     record.processCode = 'MP';
 
-    await service.getCopyOfRecord(
-      new SubmissionSet(),
-      record,
-      mockDocuments,
-      [],
-    );
+    const set = new SubmissionSet();
+    set.facIdentifier = 1;
+    set.monPlanIdentifier = 'mockMP';
+
+    await service.getCopyOfRecord(set, record, mockDocuments, []);
 
     expect(mockDocuments.length).toBe(1);
-    expect(mockDocuments[0]).toEqual('mockReport');
+  });
+
+  it('should set record status code correctly for files sent in', async () => {
+    const mockMP = new MonitorPlan();
+    const mockQaT = new QaSuppData();
+    const mockEm = new EmissionEvaluation();
+
+    jest.spyOn(service, 'returnManager').mockReturnValue({
+      findOne: jest.fn().mockImplementation((val) => {
+        switch (val.name) {
+          case 'MonitorPlan':
+            return mockMP;
+          case 'QaSuppData':
+            return mockQaT;
+          case 'EmissionEvaluation':
+            return mockEm;
+        }
+        return false;
+      }),
+
+      save: jest.fn(),
+    });
+
+    const set = new SubmissionSet();
+    set.facIdentifier = 1;
+    set.monPlanIdentifier = 'mockMP';
+
+    const record1 = new SubmissionQueue();
+    record1.processCode = 'MP';
+
+    const record2 = new SubmissionQueue();
+    record2.processCode = 'QA';
+    record2.testSumIdentifier = 'mock';
+
+    const record3 = new SubmissionQueue();
+    record3.processCode = 'EM';
+    record3.rptPeriodIdentifier = 1;
+
+    await service.setRecordStatusCode(
+      set,
+      [record1, record2, record3],
+      'COMPLETE',
+      '',
+      'UPDATED',
+    );
+
+    expect(mockMP.submissionAvailabilityCode).toEqual('UPDATED');
+    expect(mockQaT.submissionAvailabilityCode).toEqual('UPDATED');
+    expect(mockEm.submissionAvailabilityCode).toEqual('UPDATED');
   });
 
   it('should process submission set fires correctly', async () => {
     jest.spyOn(service, 'getCopyOfRecord').mockResolvedValue();
-    jest.spyOn(fsFunctions, 'writeFile').mockImplementation(() => {});
+    jest.spyOn(fsFunctions, 'writeFile').mockImplementation(jest.fn());
+    jest.spyOn(fsFunctions, 'mkdirSync').mockImplementation(jest.fn());
+    jest.spyOn(fsFunctions, 'writeFileSync').mockImplementation(jest.fn());
+    jest.spyOn(fsFunctions, 'readdirSync').mockReturnValue([]);
+    jest.spyOn(fsFunctions, 'createReadStream').mockImplementation();
+    jest.spyOn(fsFunctions, 'rmSync').mockImplementation();
+
+    jest.spyOn(service, 'setRecordStatusCode').mockImplementation(jest.fn());
 
     await service.processSubmissionSet('');
 
