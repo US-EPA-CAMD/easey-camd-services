471a146d9a3edd29f4b1f0b5ff4b03211eb9efd5
Kyle-Herceg <kyleherceg@cvpcorp.com>
2023-09-12 10:50:03 -0400

    Added workspace endpoint

--- src/copy-of-record/copy-of-record.module.ts
+++ src/copy-of-record/copy-of-record.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CopyOfRecordService } from './copy-of-record.service';
 import { CopyOfRecordController } from './copy-of-record.controller';
 import { DataSetModule } from '../dataset/dataset.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [DataSetModule],
+  imports: [DataSetModule, HttpModule],
   controllers: [CopyOfRecordController],
   providers: [CopyOfRecordService],
   exports: [CopyOfRecordService],
