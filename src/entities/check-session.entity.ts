c7870de365042ce926add1c1a9d0987583c19126
Kyle-Herceg <kyleherceg@cvpcorp.com>
2023-09-25 10:36:08 -0400

    Added severity code and end stage time

--- src/entities/check-session.entity.ts
+++ src/entities/check-session.entity.ts
@@ -5,6 +5,21 @@ export class CheckSession extends BaseEntity {
   @PrimaryColumn({ name: 'chk_session_id' })
   id: string;
 
+  @Column({ name: 'mon_plan_id' })
+  monPlanId: string;
+
+  @Column({ name: 'test_sum_id' })
+  tesSumId: string;
+
+  @Column({ name: 'qa_cert_event_id' })
+  qaCertEventId: string;
+
+  @Column({ name: 'test_extension_exemption_id' })
+  testExtensionExemptionId: string;
+
+  @Column({ name: 'rpt_period_id' })
+  rptPeriodId: number;
+
   @Column({ name: 'severity_cd' })
   severityCode: string;
 }
