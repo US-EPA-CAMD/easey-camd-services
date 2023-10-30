ca3a4e70c89825a7e5832396f719a1dd4c062032
Bill Miller <billmiller@cvpcorp.com>
2023-10-30 14:19:25 -0400

    Fix DTO Validation for Em Sub Access OrisCode

--- src/dto/em-submission-access.params.dto.ts
+++ src/dto/em-submission-access.params.dto.ts
@@ -11,6 +11,7 @@ import { Plant } from '../entities/plant.entity';
 import { IsValidCode, IsInRange } from '@us-epa-camd/easey-common/pipes';
 import { MonitorPlan } from '../entities/monitor-plan.entity';
 import { currentDateTime } from '@us-epa-camd/easey-common/utilities/functions';
+import {FindOneOptions} from "typeorm/find-options/FindOneOptions";
 
 export class EmSubmissionAccessParamsDTO {
   @ApiProperty()
@@ -19,6 +20,12 @@ export class EmSubmissionAccessParamsDTO {
     message: (args: ValidationArguments) => {
       return `The ${args.property} is not valid. Refer to the list of available facilityRecordIds for valid values '/facilities-mgmt/facilities'`;
     },
+  },(args: ValidationArguments): FindOneOptions<Plant> => {
+    return {
+      where: {
+        orisCode: args.value,
+      },
+    };
   })
   @Type(() => Number)
   orisCode?: number;
