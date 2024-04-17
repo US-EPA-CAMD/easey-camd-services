6b6a0d842ea3f77c9dbe4c46d5a515fcf871fd87
Jon Maxwell Diebold <max.diebold@erg.com>
2024-04-16 23:44:44 -0400

    Revert "fix: updated IsValidCode calls"
    This reverts commit 3089a4afc124bb504096b0f5820e8fc9f520daa6.

--- src/dto/em-submission-access.params.dto.ts
+++ src/dto/em-submission-access.params.dto.ts
@@ -2,8 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import {
   IsOptional,
   ValidationArguments,
-  IsEnum,
-  IsNotEmpty,
+  IsEnum, IsNotEmpty,
 } from 'class-validator';
 
 import { Status } from '../enums/status.enum';
@@ -12,40 +11,32 @@ import { Plant } from '../entities/plant.entity';
 import { IsValidCode, IsInRange } from '@us-epa-camd/easey-common/pipes';
 import { MonitorPlan } from '../entities/monitor-plan.entity';
 import { currentDateTime } from '@us-epa-camd/easey-common/utilities/functions';
-import { FindOneOptions } from 'typeorm/find-options/FindOneOptions';
+import {FindOneOptions} from "typeorm/find-options/FindOneOptions";
 
 export class EmSubmissionAccessParamsDTO {
   @ApiProperty()
-  @IsNotEmpty({ message: () => 'Facility is required' })
-  @IsValidCode(
-    Plant,
-    {
-      message: (args: ValidationArguments) => {
-        return `The ${args.property} is not valid. Refer to the list of available facilityRecordIds for valid values '/facilities-mgmt/facilities'`;
+  @IsNotEmpty({message: () => 'Facility is required'})
+  @IsValidCode(Plant, {
+    message: (args: ValidationArguments) => {
+      return `The ${args.property} is not valid. Refer to the list of available facilityRecordIds for valid values '/facilities-mgmt/facilities'`;
+    },
+  },(args: ValidationArguments): FindOneOptions<Plant> => {
+    return {
+      where: {
+        orisCode: args.value,
       },
-    },
-    (args: ValidationArguments): FindOneOptions<Plant> => {
-      return {
-        where: { orisCode: args.value },
-      };
-    },
-  )
+    };
+  })
   @Type(() => Number)
   orisCode?: number;
 
   @ApiProperty()
-  @IsNotEmpty({ message: () => 'Configuration is required' })
-  @IsValidCode(
-    MonitorPlan,
-    {
-      message: (args: ValidationArguments) => {
-        return `The reported ${args.property} is invalid.`;
-      },
+  @IsNotEmpty({message: () => 'Configuration is required'})
+  @IsValidCode(MonitorPlan, {
+    message: (args: ValidationArguments) => {
+      return `The reported ${args.property} is invalid.`;
     },
-    (args: ValidationArguments): FindOneOptions<MonitorPlan> => {
-      return { where: { monPlanIdentifier: args.value } };
-    },
-  )
+  })
   monitorPlanId?: string;
 
   @IsOptional()
--- src/dto/error-suppressions-payload.dto.ts
+++ src/dto/error-suppressions-payload.dto.ts
@@ -14,8 +14,6 @@ import {
   IsValidCodes,
   IsValidDate,
 } from '@us-epa-camd/easey-common/pipes';
-import { FindOneOptions } from 'typeorm';
-
 import { Plant } from '../entities/plant.entity';
 import { EsReasonCode } from '../entities/es-reason-code.entity';
 import { CheckCatalogResult } from '../entities/check-catalog-result.entity';
@@ -61,17 +59,11 @@ export class ErrorSuppressionsPayloadDTO {
   severityCode: string;
 
   @IsNumber()
-  @IsValidCode(
-    Plant,
-    {
-      message: (args: ValidationArguments) => {
-        return `The ${args.property} is not valid. Refer to the list of available facilityRecordIds for valid values '/facilities-mgmt/facilities'`;
-      },
+  @IsValidCode(Plant, {
+    message: (args: ValidationArguments) => {
+      return `The ${args.property} is not valid. Refer to the list of available facilityRecordIds for valid values '/facilities-mgmt/facilities'`;
     },
-    (args: ValidationArguments): FindOneOptions<Plant> => {
-      return { where: { facIdentifier: args.value } };
-    },
-  )
+  })
   @IsOptional()
   facilityId?: number;
 
