aee5b53ea4930e894b9e761bd84fdf728542a9ff
Evan Sun <evansun220@gmail.com>
2025-04-08 15:30:51 -0400

    feature/#6647 set the isMats default value as false and update affected tests

--- src/submission/recipient-list.service.ts
+++ src/submission/recipient-list.service.ts
@@ -61,7 +61,7 @@ export class RecipientListService {
   async getEmailRecipients(
     userId: string,
     submissionType: string,
-    isMats: boolean,
+    isMats: boolean = false,
     emailType: string = 'SUBMISSIONCONFIRMATION',
     plantId: string = '0',
   ): Promise<string> {
